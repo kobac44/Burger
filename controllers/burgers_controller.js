dd59503d8ffbefdd84b6f0d8ee17ed9c3d20dc70
Unko <kobie.watkins@gmail.com>
2020-08-19 14:03:39 -0400

    created and set-up the controllers folder and burger file within it, completed the burger file get and post set up.. but thats only coded until its test with the html

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// get routes to the index file
+router.get("/", function (req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function (req, res) {
+    // callback response, getting all the burger names back
+    burger.all(function (burgerData) {
+        //  transitional function using orm.js to connect MySql query
+        res.render("index", { burger_data: burgerData });
+    });
+});
+// post route back to the index file
+router.post("burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+// post route back to the index file
+router.put("/burgers/:id", function (req, res) {
+    burger.update(req.params.id, function (result) {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
