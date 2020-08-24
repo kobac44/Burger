2e4123cbc1f55da865209d06ea2b674e0e02c6e1
Unko <kobie.watkins@gmail.com>
2020-08-24 00:18:06 -0400

    assistant on line added some code they thought would be helpful

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("../config/connection");
 
 // creating the methods that will execute the necessary MySQL commands in the controller
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
@@ -21,6 +21,12 @@ function objToSql(ob) {
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
         var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
     }
     // translate array of strings to a single comma-separated string
     return arr.toString();
