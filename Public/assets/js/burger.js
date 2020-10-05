798015325c30348f254c0bb66a7f9ef760cb1f21
Unko <kobie.watkins@gmail.com>
2020-10-05 01:32:02 -0400

    added more code to start the finishing css and functionality of this app

---
+++ Public/assets/js/burger.js
@@ -0,0 +1,34 @@
+$(function () {
+  // burger to database click event
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    let burgerbox = {
+      burger_name: $("#burgerbox").val().trim(),
+      devoured: 0,
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: burgerbox,
+    }).then(() => {
+      // had more help with this (reload page) hardest aspects for me coding
+      location.reload();
+    });
+  });
+  //handle click event to Update the burgers to devoured on the frontend and db
+  $(".devour-burger").on("click", function (event) {
+    event.preventDefault();
+    let id = $(this).data("id");
+    let isdevoured = {
+      devoured: 1,
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: isdevoured,
+    }).then(() => {
+      console.log("change to devour to", newDevour);
+      // reload the page
+      location.reload();
+    });
+  });
+});
